a2364c03a994e72449a0f938792ca377b331cef9
tinklegames <n.awsomerlss@gmail.com>
2026-09-06 07:41:34 -0400

    fix

--- browser-config.js
+++
@@ -1,2 +0,0 @@
-// Set this to the HTTPS address where you run proxy-server (a separate origin).
-window.TINKLE_BROWSER_URL = '';
--- proxy-server/public/sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('/uv/uv.bundle.js', '/uv.config.js', '/uv/uv.sw.js');
-const uv = new UVServiceWorker();
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => {
-  if (uv.route(event)) event.respondWith(uv.fetch(event));
-});
